17fa7b11057fa7ae6f697978ce7619b6a276a4ce
Kwaltz Buck <kwaltzbu@se-d3.nsk.21-school.ru>
2022-07-28 14:54:20 +0700

    final LPD

--- src/1984.c
+++ src/1984.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <math.h>
 
@@ -10,7 +11,10 @@ int main() {
     printf("n/a");
     return 0;
   }
-  printf("%lld", lpd(x));
+  if (x == 0)
+    printf("n/a");
+  else
+    printf("%lld", lpd(x));
 }
 
 int mod(long long int x, long long int denum) {
@@ -23,16 +27,23 @@ int mod(long long int x, long long int denum) {
 
 int isPrime(long long denum) {
   if (denum > 1)
-    for (int i = 2; i < denum/2 + 1; i++) {
+    for (int i = 2; i < denum / 2 + 1; i++) {
       if (mod(denum, i)) return 0;
     }
   return 1;
 }
 
 long long int lpd(long long int x) {
-    if (x < 0)
-        x*=-1;
-  for (long long int denum = x; denum >= x/2 + 1; denum--)
+  if (x < 0) x *= -1;
+  for (long long int denum = x; denum >= x / 2 + 1; denum--)
     if (mod(x, denum) == 1)
       if (isPrime(denum)) return denum;
 }
+
+
+Format!
+Google
+Style:
+C++ online code formatter © 2014 by KrzaQ
+
+Powered by vibe.d, the D language and clang-format
