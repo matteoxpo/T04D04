82f0134cbe9a4304943019a4e0e8a893bba5cff5
Kwaltz Buck <kwaltzbu@se-d3.nsk.21-school.ru>
2022-07-29 10:59:11 +0700

    added graphics

--- src/door_functions.c
+++ src/door_functions.c
@@ -4,9 +4,11 @@
 long double Verz(long double x);
 long double Bern(long double x);
 long double Par(long double x);
+
+void print_graph(long double (*f)(long double));
+
 int main() {
     long double step = 2*M_PI/41;
-    int count = 0;
     for (long double i = -M_PI; i <= M_PI + 0.001; i+=step) {
         printf("%.7Lf |", i);
         printf(" %.7Lf |", Verz(i));
@@ -22,7 +24,13 @@ int main() {
         }
         printf("\n");
     }
+    
+    print_graph(Verz);
+    print_graph(Bern);
+    print_graph(Par);
+    
     return 0;
+    
 }
 
 long double Verz(long double x) {
@@ -35,4 +43,28 @@ long double Bern(long double x) {
 
 long double Par(long double x) {
     return 1/pow(x, 2);
+}
+
+void print_graph(long double (*f)(long double)) {
+    printf("\n-------------------------------\n");
+    long double step = 2 * M_PI/41;
+    long double max = -INFINITY;
+    long double min = INFINITY;
+    for (long double i = -M_PI; i <= M_PI + 0.001; i += step) {
+        long double y = f(i);
+        max = y > max ? y : max;
+        min = y < min ? y : min;
+    }
+    long double norma = (min + max)/21;
+    for (int i = 0; i < 21; i++){
+        for (long double x = -M_PI; x <= M_PI + 0.001; x += step) {
+            long double y = f(x);
+            if (y > (max - (i + 1) * norma) && y <= (max - i * norma))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n-------------------------------");
 }
